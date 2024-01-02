6c49bae7b73c1841ae67b739fdb157d5d0151b8c
Thea Heinen <theinen@protonmail.com>
2024-01-02 13:48:46 -0500

    add signal-desktop-twitter

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
-  description = "Nix packages which are not, for whatever reason, ready to be merged into nixpkgs";
+  description =
+    "Nix packages which are not, for whatever reason, ready to be merged into nixpkgs";
 
   outputs = { self, nixpkgs }: {
     overlay = final: prev: {
       binaryninja = prev.pkgs.callPackage ./pkgs/binaryninja.nix { };
       radius2 = prev.pkgs.callPackage ./pkgs/radius2.nix { };
+      signal-desktop-twitter = prev.pkgs.callPackage ./pkgs/signal-desktop-twitter.nix {};
     };
-    
-    packages.x86_64-linux = let pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
-    in
-    {
-      binaryninja = pkgs.callPackage ./pkgs/binaryninja.nix {};
-      radius2 = pkgs.libsForQt5.callPackage ./pkgs/radius2.nix {};
-    };
+
+    packages.x86_64-linux =
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in {
+        binaryninja = pkgs.callPackage ./pkgs/binaryninja.nix { };
+        radius2 = pkgs.callPackage ./pkgs/radius2.nix { };
+        signal-desktop-twitter = pkgs.callPackage ./pkgs/signal-desktop-twitter.nix {};
+      };
 
   };
 }
